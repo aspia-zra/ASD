98bfdcfcab80d5b1e92793ca3390258ca7fd7f3f
Magicst4r <magicalmay23@gmail.com>
2026-04-13 11:25:15 +0100

    DB connection

---
+++ db/db.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Dragonfruit23**",
+        database="secondverparagonapartment"
+    )
+
+def getconnection():
+    return get_connection()
