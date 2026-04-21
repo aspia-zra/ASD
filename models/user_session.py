98c3507faf5d73d49042c0cebd484f7e2590a007
Magicst4r <magicalmay23@gmail.com>
2026-04-21 01:02:44 +0100

    Refactor user session handling in user_session.py
    Refactor user session management functions and variables.

--- models/user_session.py
+++ models/user_session.py
@@ -1,7 +1,32 @@
+
+user_type = ""
+user_base = None  
+user_id = None
+user_name = ""
+logged_in = False
+current_user_id = None
+
+def user_sessions(user):
+    global user_type, user_base, user_id, user_name, logged_in, current_user_id
+    user_type = user.get("Role", "")
+    user_base = user.get("locationID", None)
+    user_id = user.get("userID", None)
+    user_name = user.get("fullName", "")
+    logged_in = True
+    current_user_id = user.get("userID", None)
+
+def clear():
+    global user_type, user_base, user_id, user_name, logged_in, current_user_id
+    user_type = ""
+    user_base = None
+    user_id = None
+    user_name = ""
+    logged_in = False
+    current_user_id = None
 # Save current logged in user details
 # When some logs in, this page details are updated to the new logged in user
 # All done in logging/out page
-from . import dbfunc
+from db import db
 logged_in = False
 current_user_id = None
 current_user_name = ""
